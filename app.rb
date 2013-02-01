7e64504854b18a1b04a3b75c3b83e98d553a39ec
Ken Matsuoka <matuken@ca2.so-net.ne.jp>
2013-02-02 05:27:46 +0900

    20130201 CRUD

--- app.rb
+++ app.rb
@@ -7,13 +7,17 @@ end
 
 Sequel::Model.plugin(:schema)
 
-Sequel.sqlite('db/kakeiboMemo.db')
+Sequel.sqlite('db/shuppiMemo.db')
 class Entries < Sequel::Model
 	plugin :timestamps, :update_on_create => true
 	unless table_exists?
 		set_schema do
-			primary_kei :id
+			primary_key :id
 			string :mokuteki
+			date  :date
+			int :kingaku
+			string :currency
+			string :siharaisaki
 			timestamp :created_at
 			timestamp :updated_at
 		end
@@ -22,21 +26,65 @@ class Entries < Sequel::Model
 end
 
 get '/' do
-	@index = 'kakeiboMemo - TOP'
+	@index = 'shuppiMemo - TOP'
 	@entries = Entries.all
 	haml :index
 end
 
+post '/meisai' do
+	@index = 'shuppiMemo - meisai'
+	@id = params[:id]
+	@mokuteki = params[:mokuteki]
+	@date = params[:date]
+	@kingaku = params[:kingaku]
+	@currency = params[:currency]
+	@siharaisaki = params[:siharaisaki]
+	haml :meisai
+end
+
+post '/delkaku' do
+	@index = 'shuppiMemo - delete'
+	@id = params[:id]
+	@mokuteki = params[:mokuteki]
+	@date = params[:date]
+	@kingaku = params[:kingaku]
+	@currency = params[:currency]
+	@siharaisaki = params[:siharaisaki]
+	haml :delkaku
+end
+
+
 post '/add' do
-	@entry = Entries.new :mokuteki => Sanitize.clean(params[:mokuteki])
+	@entry = Entries.new  \
+	                     :mokuteki => Sanitize.clean(params[:mokuteki]), :date => Sanitize.clean(params[:date]), \
+                         :kingaku => Sanitize.clean(params[:kingaku]), :currency => Sanitize.clean(params[:currency]), \
+						 :siharaisaki => Sanitize.clean(params[:siharaisaki])
+
 	@entry.save
 	redirect '/'
 end
 
+post '/update' do
+	@entry = Entries[params[:id]]
+	@entry.set({ :mokuteki => Sanitize.clean(params[:mokuteki]), :date => Sanitize.clean(params[:date]), \
+                 :kingaku => Sanitize.clean(params[:kingaku]), :currency => Sanitize.clean(params[:currency]), \
+		    	 :siharaisaki => Sanitize.clean(params[:siharaisaki]) })
+
+	@entry.save
+	redirect '/'
+end
+
+post '/delete' do
+	@entry = Entries[params[:id]]
+	@entry.destroy
+	redirect '/'
+end
+
+
 helpers do
 	def timefmt(time)
 		if time
-			time.strftime("%Y-%m-%d %H:%M:%S")
+			time.strftime("%Y-%m-%d")
 		else
 			''
 		end
